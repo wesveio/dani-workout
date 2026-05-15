a81aab79b39115dd14e29781e235668117066f99
Wesley Silva <wdasilva@cae.com>
2026-05-15 18:40:53 -0300

    chore(redesign): barrel export for redesign components

---
+++ src/components/redesign/index.ts
@@ -0,0 +1,13 @@
+// src/components/redesign/index.ts
+export { BottomTabBar } from './BottomTabBar';
+export { PrimaryCTA } from './PrimaryCTA';
+export { ExerciseHero } from './ExerciseHero';
+export { ExerciseThumb } from './ExerciseThumb';
+export { ExerciseSilhouette } from './ExerciseSilhouette';
+export { AderenciaDots } from './AderenciaDots';
+export type { DayState } from './AderenciaDots';
+export { PhaseStrip } from './PhaseStrip';
+export { Sparkline } from './Sparkline';
+export { MiniBars } from './MiniBars';
+export { MetricCard } from './MetricCard';
+export { ProfileRow } from './ProfileRow';
